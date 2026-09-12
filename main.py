78020be64a0702035c0c2e9c345e7ae5d140445c
tunteeyaI <ijlal.teeya@gmail.com>
2026-09-12 12:46:53 +0000

    Verify email function

--- main.py
+++ main.py
@@ -61,3 +61,51 @@ def login():
             flash("Email or Password incorrect!")
     #display html file
     return render_template("index.html")
+#signup function
+@app.route("/signup",methods=["GET", "POST"])
+def signup():
+    #check if the form is submitted
+    if request.method=="POST":
+        #get the user details
+        name=request.form["name"]
+        email=request.form["email"].lower()
+        password=request.form["password"] 
+        confirm_password=request.form["confimpassword"]
+        if not name or not email or not password:
+            flash("Fill all forms please!")
+        elif password!=confirm_password:
+            flash("Password has to match")
+        elif len(password)<6:
+            flash("Password must be atleast 6 characters")
+        else:
+            #create a verification token
+            verificationtoken=secrets.token_urlsafe(20)
+            try:
+                #add user into the database
+                with get_db() as container:
+                    container.execute("""INSERT INTO users(name,email,password_hash,verification_token)VALUES(?,?,?,?)""",(name,email,generate_password_hash(password),verificationtoken))
+                flash("Account has been created. Please check your email to verify your account")
+                #open verification page
+                return redirect(url_for("verify_email",email=email,token=verificationtoken))
+            except sqlite3.IntegrityError:
+                #email already exits
+                flash("This email has already been registered!")
+    #didplay sign up page
+    return render_template("signup.html2")
+#verify email
+@app.route("/verify-email",methods=["GET","POST"])
+def verify_email():
+    #get the email and token
+    email=request.args.get("email",request.form.get("email","")).lower()
+    token=request.args.get("token",request.form.get("token",""))
+    #check if the form was submitted
+    if request.method=="POST":
+        #update the status of verified
+        with get_db() as connection:
+            #verify user
+            updated=connection.execute("""UPDATE users SET verified=1,verified_token=NULL WHERE email=? AND verification_token=?""",(email,token)).rowcount
+        if updated:
+            flash("Email has been verified. Able to login")
+            return redirect(url_for("login"))
+        flash("Invalid verification link")
+    return render_template("verifyemail.html",email=email,token=token)
